044179f96932cdcbfc560d180580f18770881b85
sunmin <dlatjsals123@naver.com>
2025-06-07 00:34:48 +0900

    마이페이지에서 게시글 클릭 시 해당 게시글로 이동하도록 수정

--- src/views/my-page/HowToStudyList.tsx
+++ src/views/my-page/HowToStudyList.tsx
@@ -2,11 +2,13 @@ import {Get} from "@/axios.ts";
 import {HowtoStudy, HowToStudyData} from "@/views/HowToStudy.tsx";
 import {useEffect, useState} from "react";
 import Pagination from "@/components/Pagination.tsx";
+import {useNavigate} from "react-router-dom";
 
 const HowToStudyList = ()=>{
     const [page, setPage] = useState(0);
     const [list , setList] = useState<HowToStudyData[]>([]);
     const [totalPage, setTotalPage] = useState(0);
+    const navigate = useNavigate();
 
     const getHowToStudyList =async ()=>{
         const result = await Get<{ data: { data:HowtoStudy } }>(`/v1/myInfo/knowhow?size=10&page=${page+1}&sort=RegDt,DESC`)
@@ -23,7 +25,7 @@ const HowToStudyList = ()=>{
     return <div>
         <div className={'flex flex-col gap-[10px]'}>
             {
-                list.map((post) => <div className={'flex items-center gap-[10px] bg-white border border-[#E6E6E6] px-[16px] py-[10px] rounded-[20px]'} onClick={ ()=>console.log(post.knowHowSid)}>
+                list.map((post) => <div className={'flex items-center gap-[10px] bg-white border border-[#E6E6E6] px-[16px] py-[10px] rounded-[20px] cursor-pointer'} onClick={ ()=>navigate(`/detail/${post.knowHowSid}?page=how-to-study`)}>
                         <div className={'w-[60px] h-[60px] rounded-[12px] overflow-hidden'}>{post.fileList[0].srcPath ? <img src={'http://ec2-3-39-191-102.ap-northeast-2.compute.amazonaws.com:8080'+post.fileList[0].srcPath} alt={'image'} className={'w-full h-full'}/>:<div className={'w-[60px] h-[60px] rounded-[12px] bg-[#D9D9D9]'}/>}</div>
                         <div><p>{post.title}</p><p className={'truncate w-[769px]'}>{post.content}</p></div>
                     </div>
--- src/views/my-page/MemoryStudyList.tsx
+++ src/views/my-page/MemoryStudyList.tsx
@@ -2,12 +2,14 @@ import {useEffect, useState} from "react";
 import {Get} from "@/axios.ts";
 import {StudyPost, StudyPostData} from "@/views/MemoryStudy.tsx";
 import Pagination from "@/components/Pagination.tsx";
+import {useNavigate} from "react-router-dom";
 
 
 const MemoryStudyList = ()=>{
     const [page, setPage] = useState(0);
     const [list , setList] = useState<StudyPostData[]>([]);
     const [totalPage, setTotalPage] = useState(0);
+    const navigate = useNavigate();
 
     const getMemoryStudyList =async ()=>{
         const result = await Get<{ data: { data:StudyPost } }>(`/v1/myInfo/certification?size=10&page=${page+1}&sort=RegDt,DESC`)
@@ -24,7 +26,7 @@ const MemoryStudyList = ()=>{
     return <div>
         <div className={'flex flex-col gap-[10px]'}>
             {
-                list.map((post) => <div className={'flex items-center gap-[10px] bg-white border border-[#E6E6E6] px-[16px] py-[10px] rounded-[20px]'} onClick={ ()=>console.log(post.boardSid)}>
+                list.map((post) => <div className={'flex items-center gap-[10px] bg-white border border-[#E6E6E6] px-[16px] py-[10px] rounded-[20px] cursor-pointer'} onClick={ ()=> navigate(`/detail/${post.boardSid}?page=memory-study`)}>
                         <div className={'w-[60px] h-[60px] rounded-[12px] overflow-hidden'}>{post.fileList[0].srcPath ? <img src={'http://ec2-3-39-191-102.ap-northeast-2.compute.amazonaws.com:8080'+post.fileList[0].srcPath} alt={'image'} className={'w-full h-full'}/>:<div className={'w-[60px] h-[60px] rounded-[12px] bg-[#D9D9D9]'}/>}</div>
                         <div><p>{post.title}</p><p className={'truncate w-[769px]'}>{post.content}</p></div>
                     </div>
